2b078c03588f776c3c94509e634d5b66cdc0dcb8
Chris Fernandes <cjf.fernandes7@gmail.com>
2021-10-22 12:53:08 -0700

    Changes

--- apps/todo/src/main.tsx
+++ apps/todo/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import store from "./redux/store";
 
+import store from "./redux/store";
 import App from './app/App';
 
 ReactDOM.render(
